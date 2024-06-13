2ef9c262112ffa3da6451c2097e1a2cd739fd662
Dmitry Trofimov <dimkaboin@yandex.ru>
2024-06-14 01:13:20 +0300

    add github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     <>
     {app.version ? (
       <div className="h-full w-full text-center pt-4">
+        <code className="dark:text-white">{app.colorScheme}</code>
         <h3 className="font-bold mb-1 text-xl">Welcome {app.initDataUnsafe.user?.first_name}!</h3>
         <div className="font-medium text-sm text-center">I&apos;m Mini App for Telegram</div>
         <a className="mt-6 block text-lg text-cyan-500 font-bold" href="https://t.me/thismisterit">My Telegram Channel</a>
